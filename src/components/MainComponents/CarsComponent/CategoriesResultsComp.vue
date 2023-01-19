fe20d0dd75e2eb400f3bdba663d5e2ee4df1792f
MonicaDeBona <debona.monica@outlook.it>
2023-01-20 00:42:16 +0100

    Add categoryResults and singleResult components

---
+++ src/components/MainComponents/CarsComponent/CategoriesResultsComp.vue
@@ -0,0 +1,96 @@
+<script>
+import { store } from '../../../store';
+import SingleResultComp from './SingleResultComp.vue';
+
+export default {
+    name: 'CategoriesResultsComp',
+    components: {
+        SingleResultComp,
+    },
+    data() {
+        return {
+            store,
+            cardsList: [
+                {
+                    path: store.getImagePath('bmw-8-series-convertible-technical-data-sp-desktop-400x300.jpg'),
+                    model: 'BMW M9',
+                    type: 'Cabrio',
+                    price: '34.000',
+                    brand: 'BMW',
+                    fuel: 'Electric',
+                },
+                {
+                    path: store.getImagePath('bmw88-400x300.jpg'),
+                    model: 'BMW M8',
+                    type: 'Cabrio',
+                    price: '34.000',
+                    brand: 'BMW',
+                    fuel: 'Diesel',
+                },
+                {
+                    path: store.getImagePath('bmw442-400x300.jpg'),
+                    model: 'BMW 4',
+                    type: 'Cabrio',
+                    price: '34.000',
+                    brand: 'BMW',
+                    fuel: 'Gasoline',
+                },
+                {
+                    path: store.getImagePath('yeni-ibiza-2-400x300.jpg'),
+                    model: 'Seat Ibiza',
+                    type: 'Hatchback',
+                    price: '27.000',
+                    brand: 'Seat',
+                    fuel: 'Electric',
+                },
+                {
+                    path: store.getImagePath('makyajli-2022-seat-arona-1-400x300.jpg'),
+                    model: 'Seat Arona',
+                    type: 'SUV',
+                    price: '68.000',
+                    brand: 'Seat',
+                    fuel: 'Gasoline',
+                },
+                {
+                    path: store.getImagePath('vw-taigo3832-400x300.jpg'),
+                    model: 'Volkswagen Taigo',
+                    type: 'SUV',
+                    price: '52.000',
+                    brand: 'Volkswagen',
+                    fuel: 'Electric',
+                },
+                {
+                    path: store.getImagePath('2019-vw-t-cross-400x300.jpg'),
+                    model: 'Volkswagen T-Cross',
+                    type: 'SUV',
+                    price: '47.000',
+                    brand: 'Volkswagen',
+                    fuel: 'Diesel',
+                },
+                {
+                    path: store.getImagePath('volkswagen-passat-1280x720-1-400x300.jpg'),
+                    model: 'Volkswagen Passat',
+                    type: 'Sedan',
+                    price: '32.000',
+                    brand: 'Volkswagen',
+                    fuel: 'Diesel',
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="category-results d-flex main-wrapper f-wrap justify-content-between">
+        <SingleResultComp v-for="card in cardsList" :carPath="card.path" :carModel="card.model" :carPrice="card.price"
+            :carType="card.type" :carBrand="card.brand" :carFuel="card.fuel" />
+    </div>
+</template>
+
+<style lang="scss">
+.category-results {
+    padding-top: 2rem;
+    row-gap: 1.5rem;
+}
+</style>
---
+++ src/components/MainComponents/CarsComponent/SingleResultComp.vue
@@ -0,0 +1,74 @@
+<script>
+import { store } from '../../../store';
+
+export default {
+    name: 'SingleResultComp',
+    props: [
+        'carPath',
+        'carModel',
+        'carType',
+        'carPrice',
+        'carBrand',
+        'carFuel'
+    ],
+    data() {
+        return {
+            store,
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="single-card d-flex f-column">
+        <img :src="carPath" alt="carBrand">
+        <h3>{{ carModel }} <i class="fa-solid fa-circle-check"></i></h3>
+        <h4>{{ carType }}</h4>
+        <div class="info d-flex justify-content-between">
+            <div class="span-container">
+                <span><i class="fa-solid fa-dollar-sign"></i> {{ carPrice }}</span>
+                <span><i class="fa-solid fa-car"></i> {{ carBrand }}</span>
+                <span><i class="fa-solid fa-gas-pump"></i> {{ carFuel }}</span>
+            </div>
+            <span><i class="fa-solid fa-heart"></i></span>
+        </div>
+    </div>
+
+</template>
+
+<style lang="scss">
+@use '../../../styles/partials/variables' as *;
+
+.single-card {
+    width: calc(100% / 4);
+    padding: 1rem;
+    border: none;
+    padding: 1rem;
+    box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+
+    h3,
+    h4 {
+        font-weight: 300;
+        font-size: 1rem;
+    }
+
+    h3 {
+        padding: .3rem 0;
+    }
+
+    h4 {
+        padding-bottom: .5rem;
+    }
+
+    span {
+        font-weight: 300;
+        font-size: .9rem;
+        padding-right: 1rem;
+
+        .fa-heart {
+            font-size: 1rem;
+            color: $accent-color;
+        }
+    }
+}
+</style>
