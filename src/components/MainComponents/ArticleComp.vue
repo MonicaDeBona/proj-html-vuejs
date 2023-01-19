ab17e06d9689155acc70e8ad4ef7f7ab7412d45e
MonicaDeBona <debona.monica@outlook.it>
2023-01-20 00:37:40 +0100

    Add articles and article components

---
+++ src/components/MainComponents/ArticleComp.vue
@@ -0,0 +1,42 @@
+<script>
+import { store } from '../../store';
+
+export default {
+    name: 'ArticleComp',
+    props: [
+        'articlePath',
+        'articleData',
+        'articleAuthor',
+        'articleTitle',
+        'articleSummary'
+    ],
+    data() {
+        return {
+            store,
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="article-card d-flex f-column align-items-center ">
+        <img :src="articlePath" alt="car pic">
+        <div>
+            <span>{{ articleData }}</span>
+            <span>{{ articleAuthor }}</span>
+            <h3>{{ articleTitle }}</h3>
+            <p>{{ articleSummary }}</p>
+            <button>More</button>
+        </div>
+    </div>
+
+
+</template>
+
+<style lang="scss">
+.article-card {
+    padding: 2rem;
+    box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+    justify-content: space-around;
+}
+</style>
---
+++ src/components/MainComponents/ArticlesComp.vue
@@ -0,0 +1,97 @@
+<script>
+import { store } from '../../store';
+import ArticleComp from './ArticleComp.vue';
+
+export default {
+    name: 'ArticlesComp',
+    components: {
+        ArticleComp,
+    },
+    data() {
+        return {
+            store,
+            articlesList: [
+                {
+                    path: store.getImagePath('news-autocar-1-300x180.jpg'),
+                    data: '06.07.2022',
+                    author: 'Auto Car',
+                    title: 'Who is BMW loved?',
+                    summary: 'BMWs have a different line with their designs and almost [...]'
+                },
+                {
+                    path: store.getImagePath('news-autocar-2-300x180.jpg'),
+                    data: '06.07.2022',
+                    author: 'Auto Car',
+                    title: 'How solid is Audi?',
+                    summary: 'According to Euro NCAP data with different test organizations, there [...]'
+                },
+                {
+                    path: store.getImagePath('news-autocar-5-300x180.jpg'),
+                    data: '06.07.2022',
+                    author: 'Auto Car',
+                    title: 'What brand is Skoda?',
+                    summary: 'Skoda is one of the leading automotive manufacturers on the [...]'
+                },
+                {
+                    path: store.getImagePath('news-autocar-6-300x180.jpg'),
+                    data: '06.07.2022',
+                    author: 'Auto Car',
+                    title: 'Is the Auris diesel',
+                    summary: 'You will have a quieter vechicle that burns less than [...]'
+                },
+            ]
+        }
+    },
+}
+</script>
+<template>
+    <div class="recent-articles d-flex f-column align-items-center">
+        <span class="icon-circle d-flex align-items-center justify-content-center">
+            <i class="fa-regular fa-thumbs-up"></i>
+        </span>
+        <h2>
+            Resent Articles
+        </h2>
+        <h5>
+            Useful Information about car.
+        </h5>
+    </div>
+    <div class="articles d-flex main-wrapper justify-content-around">
+        <ArticleComp v-for="article in articlesList" :articlePath="article.path" :articleData="article.data"
+            :articleAuthor="article.author" :articleTitle="article.title" :articleSummary="article.summary" />
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../../styles/partials/variables' as *;
+
+.recent-articles {
+    .icon-circle {
+        height: 110px;
+        width: 110px;
+        background-color: $secondary-color;
+        border-radius: 50%;
+        margin-bottom: 1rem;
+
+        i {
+            color: $main-color;
+            font-size: 3rem;
+        }
+    }
+
+    h2 {
+        font-size: 2rem;
+        padding-bottom: .5rem;
+    }
+
+    h5 {
+        font-weight: 300;
+        font-size: 1.1rem;
+        padding-bottom: 1.5rem;
+    }
+}
+
+// .articles {
+//     // column-gap: 2rem;
+// }
+</style>
