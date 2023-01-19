bff986be6929a7a624d42dfba8e7f1cd1aff579d
MonicaDeBona <debona.monica@outlook.it>
2023-01-20 00:38:07 +0100

    Add customers and customer components

---
+++ src/components/MainComponents/CustomerComp.vue
@@ -0,0 +1,51 @@
+<script>
+import { store } from '../../store';
+
+export default {
+    name: 'CustomerComp',
+    props: [
+        'customerText',
+        'customerPath',
+        'customerName'
+    ],
+    data() {
+        return {
+            store,
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="customer-card">
+        <p>{{ customerText }}</p>
+        <img :src="customerPath" alt="customer">
+        <h4>{{ customerName }}</h4>
+    </div>
+
+</template>
+
+<style lang="scss">
+@use '../../styles/partials/variables' as *;
+
+.customer-card {
+    margin: 2rem;
+    padding: 4rem 2.5rem;
+    text-align: center;
+    border-radius: 10px;
+    background-color: $main-color;
+
+    img {
+        width: 100px;
+        margin: 2.5rem 0;
+    }
+
+    p {
+        font-weight: 300;
+    }
+
+    h4 {
+        font-weight: 300;
+    }
+}
+</style>
---
+++ src/components/MainComponents/CustomersComp.vue
@@ -0,0 +1,62 @@
+<script>
+import { store } from '../../store';
+import CustomerComp from './CustomerComp.vue';
+export default {
+    name: 'CustomersComp',
+    components: {
+        CustomerComp,
+    },
+    data() {
+        return {
+            store,
+            customersList: [
+                {
+                    text: 'We are a gallery vehicle dealer. Before we met this site, our sales were sluggish..',
+                    path: store.getImagePath('man1.png'),
+                    name: 'Adam',
+
+                },
+                {
+                    text: 'We are a gallery vehicle dealer. Before we met this site, our sales were sluggish..',
+                    path: store.getImagePath('testimonial2-1.png'),
+                    name: 'Christie',
+
+                },
+                {
+                    text: 'We are a gallery vehicle dealer. Before we met this site, our sales were sluggish..',
+                    path: store.getImagePath('man2.png'),
+                    name: 'Robert',
+
+                },
+                {
+                    text: 'We are a gallery vehicle dealer. Before we met this site, our sales were sluggish..',
+                    path: store.getImagePath('testimonial4-1.png'),
+                    name: 'Elizabeth',
+
+                }
+            ]
+        }
+    },
+}
+</script>
+<template>
+    <div class="customers">
+        <div class=" customer-container main-wrapper d-flex justify-content-center align-items-center">
+            <CustomerComp v-for="customer in customersList" :customerText="customer.text" :customerPath="customer.path"
+                :customerName="customer.name" />
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.customers {
+    background-image: url('../../assets/imgs/comment-bg-autocar.jpg');
+    background-position: center;
+    background-size: cover;
+    height: 900px;
+
+    .customer-container {
+        height: 100%;
+    }
+}
+</style>
