64ce6ae8fd8c6de2c6e01f38cf3ce615f7eaef94
MonicaDeBona <debona.monica@outlook.it>
2023-01-19 11:59:01 +0100

    add main components

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,11 +1,22 @@
 <script>
+import SearchComp from './MainComponents/SearchComp.vue';
+import CategoriesComp from './MainComponents/CategoriesComp.vue';
+import CardComp from './MainComponents/CardCompt.vue';
 export default {
+    name: 'AppMain',
 
+    components: {
+        SearchComp,
+        CategoriesComp,
+        CardComp
+    }
 }
 </script>
 
 <template>
-
+    <SearchComp />
+    <CategoriesComp />
+    <CardComp />
 </template>
 
 <style lang="scss" scoped>
